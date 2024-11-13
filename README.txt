3a21fd3b41ad2aec1ed6e07fb4bcb3b942604251
EVAN REED <er0997669@otc.edu>
2024-11-13 15:04:19 -0600

    Final Clean up

---
+++ README.txt
@@ -0,0 +1,1 @@
+This application connects to the zenquote api to print out a random quote into the console using node.js
--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+//import axios for use
 import axios from 'axios';
+
 function GetQuote() {
+    //use axios to connect to api and pull data on a random quote
     axios.get("https://zenquotes.io/api/random").then((response) => {
         // console.log(response.status);
+        //check the status of the API request. If good, continue with the application if not print an error
         if (response.status == 200) {
+            //save the specific quote data in a variable
             var quote = response.data;
+            //print the data to the console in a pretty way
             console.log("Here is a fun quote for you!");
             console.log(quote[0].q + " - " + quote[0].a);
 
         } else {
+            //error handling and printing the specific error text from the api response
             console.log("Could not Fetch Quote, Try again later");
             console.log(response.statusText);
         }
